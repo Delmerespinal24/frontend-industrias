8cb38d509b63f9dcbe87a285da0089efa64b75c0
Delmerespinal24 <Delmerespinal2@gmail.com>
2023-03-14 00:23:25 -0600

    actualizando ruta backend

--- frontend-industria/src/app/service/administrador.service.ts
+++ frontend-industria/src/app/service/administrador.service.ts
@@ -8,7 +8,8 @@ import { Administrador } from '../interfaces/administrador';
 })
 export class AdministradorService {
   // private apiUrl = 'http://localhost:4000/signupAdmin'
-  private apiUrl = 'http://ec2-44-214-12-139.compute-1.amazonaws.com:4000/signupAdmin'
+  // private apiUrl = 'http://ec2-44-214-12-139.compute-1.amazonaws.com:4000/signupAdmin'
+  private apiUrl = 'http://ec2-52-71-59-232.compute-1.amazonaws.com:4000/signupAdmin'
 
   constructor(private http: HttpClient) { }
 
