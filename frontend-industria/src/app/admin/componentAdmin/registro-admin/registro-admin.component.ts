4b9730ec1ae274e073f23aafea5c94c0630bf80b
Delmerespinal24 <Delmerespinal2@gmail.com>
2023-03-20 01:28:30 -0600

    validaciones 1

--- frontend-industria/src/app/admin/componentAdmin/registro-admin/registro-admin.component.ts
+++ frontend-industria/src/app/admin/componentAdmin/registro-admin/registro-admin.component.ts
@@ -12,19 +12,71 @@ import { Router } from '@angular/router';
 })
 export class RegistroAdminComponent {
 
- public form!: FormGroup;
+  /*
+  public form!: FormGroup;
   registroAdmin!: Administrador;
+  */
 
-  maxN=20;
-  maxNP=10;
+  maxN=50;
 
-  
-
-  constructor(private fb: FormBuilder, private registroadmin:AdministradorService,private router:Router,) {
+  constructor(
+    /*private fb: FormBuilder,*/
+    private registroAdmin:AdministradorService, private router:Router) {
 
   }
+
+  
+  registrationForm=new FormGroup({
+      nombre: new FormControl('', [Validators.required,Validators.minLength(3), Validators.maxLength(this.maxN)]),
+      apellido: new FormControl('', [Validators.required,Validators.minLength(3), Validators.maxLength(this.maxN)]),
+      usuario: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(this.maxN), Validators.pattern('^[A-Za-z](([0-9])|[A-Za-z])+')]),
+      nacimiento: new FormControl('', [Validators.required, Validators.pattern('^\\d{4}([-])(0?[1-9]|1[0-2])\\1(3[01]|[12][0-9]|0?[1-9])')]),
+      correo: new FormControl('', [Validators.required, Validators.email]),
+      telefono:  new FormControl('',[Validators.required, Validators.pattern('[0-9]{8}')]),
+      sexo: new FormControl('',[Validators.required]),
+      clave: new FormControl('', [Validators.required,
+        /*Validators.pattern(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/)*/
+      ]),
+      admin: new FormControl('1')
+  });
+  
+  get fisrtnameControl():FormControl{
+    return this.registrationForm.get('nombre') as FormControl
+  }
+  get lastnameControl():FormControl{
+    return this.registrationForm.get('apellido') as FormControl
+  }
+  get usernameControl():FormControl{
+    return this.registrationForm.get('usuario') as FormControl
+  }
+  get birthdateControl():FormControl{
+    return this.registrationForm.get('nacimiento') as FormControl
+  }
+  get emailControl():FormControl{
+    return this.registrationForm.get('correo') as FormControl
+  }
+  get phoneControl():FormControl{
+    return this.registrationForm.get('telefono') as FormControl
+  }
+  get genderControl():FormControl{
+    return this.registrationForm.get('sexo') as FormControl
+  }
+  get passwordControl():FormControl{
+    return this.registrationForm.get('clave') as FormControl
+  }
+
+  firstname = "";
+  lastname = "";
+  username = "";
+  birthdate = "";
+  email = "";
+  phone = "";
+  gender = "";
+  password = "";
+  admin = 1;
   
 
+  /*
   name= new FormControl('', [Validators.required, Validators.maxLength(this.maxN)]);
   name2= new FormControl('', [Validators.required, Validators.maxLength(this.maxN)]);
   username= new FormControl('', [Validators.required, Validators.maxLength(this.maxN)]);
@@ -36,7 +88,16 @@ export class RegistroAdminComponent {
     /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/
   )]);
   admin= new FormControl('1');
-
+  
+  get genderControl():FormControl{
+    return this.form.get('gender') as FormControl
+  }
+  get passwordControl():FormControl{
+    return this.form.get('password') as FormControl
+  }
+  get adminControl():FormControl{
+    return this.form.get('admin') as FormControl
+  }
   
   ngOnInit(): void {
     this.form = this.fb.group({
@@ -53,28 +114,33 @@ export class RegistroAdminComponent {
     console.log('entrada', this.form.value)
   }
 
- 
+  */
+
+  
+
+  
 
   registro() {
-    if (this.form.valid) {
-      // handle form submission
-      let phone1:number = parseInt(this.phone.value as string);
-      let admin1: number = parseInt(this.admin.value as string)
 
-      this.registroAdmin=({
-        primerNombre:this.name.value!,
-        primerApellido:this.name2.value!,
-        nombreUsuario:this.username.value!,
-        FechaNacimiento: this.birthdate.value!,
-        correoElectronico :this.email.value!,
-        telefono:phone1,
-        sexo:this.gender.value!,
-        password: this.password.value!,
-        esAdmin:admin1,
-      })
+    let newAdministrador: Administrador={
+      primerNombre: "" + this.fisrtnameControl.value,
+      primerApellido: "" + this.lastnameControl.value,
+      nombreUsuario: "" + this.usernameControl.value,
+      FechaNacimiento: "" + this.birthdateControl.value,
+      correoElectronico : "" + this.emailControl.value,
+      telefono:parseInt( "" + this.phoneControl.value ),
+      sexo: "" + this.genderControl.value,
+      password: "" + this.passwordControl.value,
+      esAdmin:1
+    }
+
+    console.log(newAdministrador);
+    /*
+    if (this.registrationForm.valid) {
+      // handle form submission
   
-      console.log("new: ",this.registroAdmin)
-      this.registroadmin.newAdmin(this.registroAdmin).subscribe(res=>{
+      console.log("new: ",this.newAdministrador)
+      this.registroAdmin.newAdmin(this.newAdministrador).subscribe(res=>{
         console.log('Respuesta:',res)
       })
 
@@ -84,7 +150,7 @@ export class RegistroAdminComponent {
     } else {
       // display an error message to the user
       alert('Please fill all required fields');
-    }
+    }*/
   }
   
 }
--- frontend-industria/src/app/app-routing.module.ts
+++ frontend-industria/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistroAdminComponent } from './admin/componentAdmin/registro-admin/registro-admin.component';
+import { PerfilAdminComponent } from './admin/componentAdmin/perfil-admin/perfil-admin.component';
 import { HomeComponent } from './home/home/home.component';
 
 
@@ -8,6 +9,7 @@ import { HomeComponent } from './home/home/home.component';
 const routes: Routes = [
   { path:'', component:HomeComponent},
   { path:'registro-admin', component:RegistroAdminComponent},
+  {path:'perfil-admin', component:PerfilAdminComponent},
 ];
 
 @NgModule({
